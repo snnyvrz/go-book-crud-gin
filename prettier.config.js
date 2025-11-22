ba176cb3003724550b89536ea408e701db43d688
sinaniya <sinaniya@gmail.com>
2025-11-22 23:07:43 +0100

    fix(root): prettier config

--- prettier.config.js
+++
@@ -1,3 +0,0 @@
-/** @type {import("prettier").Config} */
-export const tabWidth = 4;
-export const trailingComma = "es5";
---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 4,
+  trailingComma: "es5",
+};
+
+export default config;
